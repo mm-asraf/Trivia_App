fd47957275ebe78369bc0cbda6cd31c197c69004
Mahboob Asraf <mahboobasraf@gmail.com>
2022-07-07 11:41:39 +0530

    update

--- backend/src/controllers/score.controller.js
+++ backend/src/controllers/score.controller.js
@@ -24,9 +24,7 @@ router.get("", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const score = await Score.findById(req.params.id, req.body, { new: true })
-      .lean()
-      .exec();
+    const score = await Score.findById(req.params.id).lean().exec();
     return res.status(200).send(score);
   } catch (err) {
     return res.status(200).json({ err: err.message, status: "failed" });
--- backend/src/models/score.model.js
+++ backend/src/models/score.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const scoreSchema = new Schema(
   {
-    scored: { type: String, type: Number },
+    scores: { type: String, type: Number },
     category: { type: String, type: String },
     player: {
       type: Schema.Types.ObjectId,
